3e60e9d747d400aa945cfcea8cafa99818be9688
Andre Verissimo <afsverissimo@gmail.com>
2011-02-27 00:36:19 +0000

    pre-live

---
+++ genesis_gastagus/nivo/nivo.js
@@ -0,0 +1,9 @@
+jQuery(function($){
+   $('#slider').nivoSlider({
+        effect:'fade', //Specify sets like: 'fold,fade,sliceDown'
+		slices:1,
+		manualAdvance:false,
+        pauseOnHover:true, //Stop animation while hovering
+		}
+   );
+});
